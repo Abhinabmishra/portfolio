6b4b05f1fa371c7457f150c3391cf659a0fc9c71
Abhinab <32684040+Abhinabmishra@users.noreply.github.com>
2026-04-13 19:58:19 +0530

    Update Projects.tsx

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -72,7 +72,7 @@ export default function Projects() {
             </motion.div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-5xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto">
           {projects.map((project, index) => (
             <div 
               key={project.title} 
